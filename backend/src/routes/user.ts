f9920b87f749a7d74bbf082f013e5bca0300b2a9
Yagyesh1 <yagyeshsingh9628@gmail.com>
2025-09-13 00:00:30 +0530

    feat: add followCategory unfollowCategory getUserCategories

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import { signup } from "../controllers/user/signup";
 import { login } from "../controllers/user/login";
+import { followCategory } from "../controllers/user/followCategory";
+import { unfollowCategory } from "../controllers/user/unfollowCategory";
+import { authMiddleware } from "../middleware/auth.middleware";
+import { getUserCategories } from "../controllers/user/getUserCategories";
 
 export const userRouter = express()
 
 userRouter.post('/signup', signup)
 userRouter.post('/login', login)
+userRouter.post('/followCategory',authMiddleware, followCategory)
+userRouter.post('/unfollowCategory',authMiddleware, unfollowCategory)
+userRouter.post('/getUserCategories',authMiddleware, getUserCategories)
